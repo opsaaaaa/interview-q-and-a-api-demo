f75f4b0c3e1412276fd8532a27ab182bad5c148d
opsaaaaa <sean@ferney.org>
2022-04-30 16:57:47 -0600

    published and privy scopes

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,7 @@ class Question < ActiveRecord::Base
   has_many :answers
   belongs_to :user
 
+  scope :published, -> { where(private: false) } 
+  scope :privy, -> { where(private: true) } 
+
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,4 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   factory_can_create(:question)
+
+  context "scopes" do
+    before  do
+      create_list :question, 3, private: true
+      create_list :question, 3, private: false
+    end
+
+    it "to private questions" do
+      Question.privy.each do |q|
+        expect(q.private).to be true
+      end
+    end
+
+    it "to public questions" do |q|
+      Question.published.each do |q|
+        expect(q.private).to be false
+      end
+    end
+
+  end
 end
