d7c8631ae9cea51dc9461f9eb3ebbc998f776fc9
opsaaaaa <sean@ferney.org>
2022-05-01 23:44:29 -0600

    answers published and privy join scopes

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,7 @@ class Answer < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
 
+  scope :published, -> { joins(:question).where(question: {private: false}) } 
+  scope :privy, -> { joins(:question).where(question: {private: true}) }
+
 end
